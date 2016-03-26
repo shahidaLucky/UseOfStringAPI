226709facda835b57d0771b4b7e1bdb143175ca4
SLucky <shahidakawsar@gmail.com>
2016-03-26 12:39:31 -0400

    initial commit for useOfStringAPI

---
+++ src/useOfStringAPI/StringTest.java
@@ -0,0 +1,24 @@
+package useOfStringAPI;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		
+		String str = "This is selenium class";
+		String s = "WORLD";
+		String s1 = "wide";
+		
+		System.out.println(str);
+		System.out.println();
+		System.out.println(s.toLowerCase());
+		System.out.println(s1.toUpperCase());
+		System.out.println(s.concat(s1));
+		System.out.println(str.charAt(3));
+		System.out.println(str.substring(3, 10));
+		System.out.println(str.length());
+		System.out.println();
+		
+
+	}
+
+}
